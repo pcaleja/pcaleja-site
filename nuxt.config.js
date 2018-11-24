e47f8b2164a782ab33be3b7a7c8bbe5b97e7c2ee
Philip Caleja <pcaleja@gmail.com>
2018-11-24 02:07:31 -0500

    set lang attribute

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ module.exports = {
   },
   head: {
     title: 'Philip Caleja',
-    htmlAttr: { locale: 'en' },
+    htmlAttr: { lang: 'en' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0' }
